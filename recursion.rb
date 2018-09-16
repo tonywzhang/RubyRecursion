d1fc2fdd3eace36bedb04220d5fd602d066cf2c6
Tony <tonywzhang@gmail.com>
2018-09-16 14:06:51 -0700

    add_numbers

--- recursion.rb
+++ recursion.rb
@@ -9,3 +9,15 @@ end
 # p sum_to(1)  # => returns 1
 # p sum_to(9)  # => returns 45
 # p sum_to(-8)  # => returns nil
+
+def add_numbers(num_array)
+  return nil if num_array.empty?
+  return num_array.first if num_array.length <= 1
+
+  num_array.pop + add_numbers(num_array)
+end
+
+# p add_numbers([1,2,3,4]) # => returns 10
+# p add_numbers([3]) # => returns 3
+# p add_numbers([-80,34,7]) # => returns -39
+# p add_numbers([]) # => returns nil
